5408f097e883dba495f50221d05237fd337eb864
Abubakr <thefox5000@gmail.com>
2024-01-21 08:50:45 +0300

    Is day last day in month, is month last month in year

---
+++ Problem #15/file.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+struct Date
+{
+    short Day, Month, Year;
+};
+
+short ReadNumber(string Msg = "Please Enter a number")
+{
+    short Number;
+    cout << Msg << endl;
+    cin >> Number;
+    return Number;
+}
+
+void ReadAndCreateDate(Date &NewDate)
+{
+    short Year = ReadNumber("Enter Year: ");
+    short Month = ReadNumber("Enter Month: ");
+    short Day = ReadNumber("Enter Day: ");
+
+    NewDate.Day = Day;
+    NewDate.Month = Month;
+    NewDate.Year = Year;
+}
+
+bool IsLeapYear(short Number)
+{
+    return (Number % 4 == 0 && Number % 100 != 0) || (Number % 400 == 0);
+}
+
+short DaysInMonth(short Year, short Month)
+{
+    int DaysOfMonths[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    return (Month < 1 || Month > 12) ? 0 : (Month == 2 ? (IsLeapYear(Year) ? 29 : 28) : (DaysOfMonths[Month - 1]));
+
+    // In One Line:
+    // return Month == 2 ? (IsLeapYear(Year) ? 29 : 28) : (Month == 4 || Month == 6 || Month == 9 || Month == 11) ? 30 : 31;
+}
+
+bool IsDayLast(Date NewDate)
+{
+    return NewDate.Day == DaysInMonth(NewDate.Year, NewDate.Month);
+}
+
+bool IsMonthLast(Date NewDate)
+{
+    return NewDate.Month == 12;
+}
+
+int main()
+{
+    bool RunLoop = true;
+
+    while (RunLoop)
+    {
+        Date NewDate1;
+        ReadAndCreateDate(NewDate1);
+
+        if (IsDayLast(NewDate1))
+        {
+            cout << "Last Day" << endl;
+        }
+        else
+        {
+            cout << "NOT Last Day" << endl;
+        }
+
+        if (IsMonthLast(NewDate1))
+        {
+            cout << "Last Month" << endl;
+        }
+        else
+        {
+            cout << "NOT Last Month" << endl;
+        }
+    }
+
+    return 0;
+}
